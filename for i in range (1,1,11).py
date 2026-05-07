0428f464c331481f5ebb8f129f75986e0a17887f
DanielGodriNeto <godrineto@gmail.com>
2026-05-07 10:59:16 -0300

    Updated the game to remove some wrong archives

--- for i in range (1,1,11).py
+++
@@ -1,7 +0,0 @@
-numero = int(input("Digite um número: "))
-while numero <= 5:
-    if numero > 5:
-     for i in range(1, numero+1):
-         print(i)
-    else:
-      print("O número deve ser maior que 5.")
